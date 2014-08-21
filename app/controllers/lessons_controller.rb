9dae3f695a70e13f66b1f26f779d4b8087418eca
J.J. Idt & Justin Speers <student@epicodus.com>
2014-08-21 14:31:55 -0700

    implement deleting

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,16 +6,43 @@ class LessonsController < ApplicationController
     render('lessons/new.html.erb')
   end
 
+  def index
+    @lessons = Lesson.all.order(:number)
+    render('lessons/index.html.erb')
+  end
+
   def create
     @lessons = Lesson.all
-    params[:lesson][:number] = params[:lesson][:number].to_i
     @lesson = Lesson.new(params[:lesson])
-    Lesson.shift_lessons_from(@lesson.number)
-    if @lesson.save
-      redirect_to("/lessons/#{@lesson.id}")
+    if @lesson.valid?
+       Lesson.shift_lessons_from(@lesson.number)
+       @lesson.save
+       redirect_to("/lessons/#{@lesson.id}")
     else
       render("lessons/new.html.erb")
     end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    render('lessons/show.html.erb')
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render('lessons/edit.html.erb')
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    @lesson.update(params[:lesson])
+    render('/lessons/show.html.erb')
+  end
+
+  def delete
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to("/")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  match('/', :via => :get, :to => 'lessons#index')
   match('/lessons/new', :via => :get, :to => 'lessons#new')
   match('/lessons/create', :via => :post, :to => 'lessons#create')
   match('/lessons/:id', :via => :get, :to => 'lessons#show')
+  match('/lessons/:id/edit', :via => :get, :to => 'lessons#edit')
+  match('/lessons/:id', :via => [:patch, :put], :to => 'lessons#update')
+  match('/lessons/:id', :via => :delete, :to => 'lessons#delete')
+
 end
